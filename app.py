63c061978227d240312476d25de8b8fb1b8b9641
Cavendish <techiechic12.34@gmail.com>
2019-09-29 22:39:08 +0300

    app_settings_2.2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+
 
 @app.route('/')
 def hello():
@@ -14,7 +14,7 @@ def hello_name(name):
     return "Hello {}!".format(name)
 
 
-print(os.environ['APP_SETTINGS'])
+# print(os.environ['APP_SETTINGS'])
 
 if __name__ == '__main__':
     app.run()
